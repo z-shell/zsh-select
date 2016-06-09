16bb0cb2f8ae414ccd8fe8423b0c3ef6f8a9f91b
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-06-09 21:24:35 +0200

    Immunity to setopt nofunctionargzero

--- zsh-select.plugin.zsh
+++ zsh-select.plugin.zsh
@@ -3,5 +3,10 @@
 # available in interactive `Zsh` sessions only when using this
 # method.
 
+# This gives immunity to functionargzero being unset
+# _ will be set to last argument to source builtin
+PLUGIN_UNDERSCORE="$_"
+[ "$0" != "$PLUGIN_UNDERSCORE" ] && 0="$PLUGIN_UNDERSCORE"
+
 REPO_DIR="${0%/*}"
 path+=( "$REPO_DIR" )
